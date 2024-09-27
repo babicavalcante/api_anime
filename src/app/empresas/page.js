a87f85d06d240c8e9cf461abfc55d1027cd1b7b4
Barbara Cirino Cavalcante <barbara.cavalcante@hcb.org.br>
2024-09-27 08:42:31 -0300

    LocalStorage

--- src/app/empresas/page.js
+++ src/app/empresas/page.js
@@ -1,17 +1,19 @@
 'use client'
 
-
-import Link from "next/link"
-import { Table } from "react-bootstrap"
-import { FaPlusCircle } from "react-icons/fa";
+import Link from "next/link";
 import Pagina from "../components/Pagina";
+import { FaPlusCircle } from "react-icons/fa";
+import { Table } from "react-bootstrap";
 
 export default function Page() {
+
+    const empresas = JSON.parse(localStorage.getItem('empresas')) || []
+
     return (
         <Pagina titulo="Empresas">
 
-            <Link 
-                href="/empresas/create" 
+            <Link
+                href="/empresas/create"
                 className="btn btn-primary mb-3"
             >
                 <FaPlusCircle /> Novo
@@ -26,21 +28,17 @@ export default function Page() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Gol</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Latam</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Azul</td>
-                        <td></td>
-                    </tr>
+                    {empresas.map(item => (
+                        <tr>
+                            <td>1</td>
+                            <td>{item.nome}</td>
+                            <td>
+                                <a href={item.site} target="_blank">
+                                    <img src={item.logo} width={100} />
+                                </a>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </Table>
         </Pagina>
