0c315501f647a016ba0956106b50e5f394d110c8
unknown <22214290017@ACADEMICO.IESB.BR>
2024-10-10 20:27:07 -0300

    id

--- src/app/voos/page.js
+++ src/app/voos/page.js
@@ -1,38 +1,74 @@
-'use client';
+'use client'
 
-import Link from "next/link";
-import { Table } from "react-bootstrap";
+import Link from "next/link"
+import { useEffect, useState } from "react";
+import { Table } from "react-bootstrap"
 import { FaPlusCircle } from "react-icons/fa";
+import { FaRegEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 import Pagina from "../components/Pagina";
 
-export default function Voos() {
-    const voos = JSON.parse(localStorage.getItem('voos')) || [];
+export default function Page() {
+
+    const [voos, setVoos] = useState([])
+
+    useEffect(() => {
+        setVoos(JSON.parse(localStorage.getItem('voos')) || [])
+    }, [])
+
+    function excluir(id) {
+        if (confirm('Deseja realmente excluir o registro?')) {
+            const dados = voos.filter(item => item.id != id)
+            localStorage.setItem('voos', JSON.stringify(dados))
+            setVoos(dados)
+        }
+    }
 
     return (
         <Pagina titulo="Voos">
-            <Link href="/voos/create" className="btn btn-primary mb-3">
+
+            <Link
+                href="/voos/form"
+                className="btn btn-primary mb-3"
+            >
                 <FaPlusCircle /> Novo
             </Link>
+
             <Table striped bordered hover>
                 <thead>
                     <tr>
                         <th>#</th>
-                        <th>Número</th>
-                        <th>Aeroporto de Origem</th>
-                        <th>Aeroporto de Destino</th>
+                        <th>Identificador</th>
+                        <th>Dt. Embarque</th>
+                        <th>Origem</th>
+                        <th>Destino</th>
+                        <th>Empresa</th>
+                        <th>Preço</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {voos.map((item, index) => (
-                        <tr key={index}>
-                            <td>{index + 1}</td>
-                            <td>{item.numero}</td>
+                    {voos.map((item, i) => (
+                        <tr key={item.id}>
+                            <td>
+                                <Link href={`/voos/form/${item.id}`}>
+                                    <FaRegEdit title="Editar" className="text-primary" />
+                                </Link>
+                                <MdDelete
+                                    title="Excluir"
+                                    className="text-danger"
+                                    onClick={() => excluir(item.id)}
+                                />
+                            </td>
+                            <td>{item.identificador}</td>
+                            <td>{item.data_embarque}</td>
                             <td>{item.origem}</td>
                             <td>{item.destino}</td>
+                            <td>{item.empresa}</td>
+                            <td>{item.preco}</td>
                         </tr>
                     ))}
                 </tbody>
             </Table>
         </Pagina>
-    );
+    )
 }
